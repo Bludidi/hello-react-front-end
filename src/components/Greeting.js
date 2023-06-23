dd00838f9f2734e3ffbf72d694b399da063eb04e
Bludidi <bludidi5@gmail.com>
2023-06-23 11:07:59 +0200

    Change state handling

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -5,19 +5,26 @@ import { getApiData } from '../redux/greetingSlice';
 const Greeting = () => {
   const dispatch = useDispatch();
   const newGreetings = useSelector((state) => state.greeting.greetingData);
+  const isLoading = useSelector((state) => state.greeting.isLoading);
 
   useEffect(() => {
     dispatch(getApiData());
   }, [dispatch]);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (newGreetings.length === 0) {
+    return <h1>No Greetings</h1>;
+  }
   return (
-    <>
-      <p>Example of Greetings</p>
+    <div>
       {newGreetings.map((greeting) => (
         <div key={greeting.id}>
           <h2>{greeting.greeting}</h2>
         </div>
       ))}
-    </>
+    </div>
   );
 };
+
 export default Greeting;
